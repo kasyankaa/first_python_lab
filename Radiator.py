78e2afd531202872548a7b902d965aaa720066ca
Christina Kasyan <55959819+kasyankaa@users.noreply.github.com>
2020-04-13 03:20:04 +0300

    More beautiful

--- Radiator.py
+++ Radiator.py
@@ -1,23 +1,28 @@
 class Radiator:
     number_of_radiators = 0
 
-    def __init__(self, thermal_power_in_watts=0, color="Default", producer="Default", wheelbase_in_mm=0, price_in_uah=0,
-                 connection_type="Default", type_of_radiator="Default"):
+    def __init__(self, name="Usual", thermal_power_in_watts=80, color="white", producer="Ukraine", wheelbase_in_mm=5,
+                 price_in_uah=432, type_of_radiator="gorgeous"):
+        self.name = name
         self.thermal_power_in_watts = thermal_power_in_watts
         self.color = color
         self.producer = producer
         self.wheelbase_in_mm = wheelbase_in_mm
         self.price_in_uah = price_in_uah
-        self.type = type_of_radiator
-        self.connection_type = connection_type
+        self.type_of_radiator = type_of_radiator
         Radiator.number_of_radiators += 1
 
     def __del__(self):
         print("Deleted radiator")
 
     def __str__(self):
-        return ', '.join((f"{name[:]} = {value}" for name, value in self.__dict__.items()))
-    __repr__ = __str__
+        return 'Radiator(name:' + self.name \
+               + ', thermal power:' + str(self.thermal_power_in_watts) \
+               + ', color:' + self.color \
+               + ', producer:' + self.producer \
+               + ", wheelbase in mm:" + str(self.wheelbase_in_mm) \
+               + ', price' + str(self.price_in_uah) \
+               + ', type=' + str(self.type_of_radiator) + ')'
 
     @staticmethod
     def create_radiator():
@@ -25,13 +30,13 @@ class Radiator:
 
 
 def main():
-    TYC = Radiator(45, "blue", "USA", 23, 232, "upper", "silver")
-    Mishimoto = Radiator(34, "white", "Germany", 34, 3456)
-    Koyo = Radiator(23, "black", "China")
+    First = Radiator("TYC", 45, "blue", "USA", 23, 232, "beautiful")
+    Second = Radiator("Mishimoto", 34, "white", "Germany", 34,)
+    Third = Radiator("Koyo",23, "black", "China")
 
-    print(TYC)
-    print(Mishimoto)
-    print(Koyo)
+    print(First)
+    print(Second)
+    print(Third)
 
     print("Number of created radiators", Radiator.number_of_radiators)
 
